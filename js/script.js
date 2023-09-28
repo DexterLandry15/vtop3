aebdef106962c0664b1e0471bc9e3e745aaf06d2
dexterlandry15 <dexter.landry15@gmail.com>
2023-09-28 18:44:47 +0300

    oy

--- js/script.js
+++ js/script.js
@@ -1,22 +1,9 @@
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
-      }
-    };
-    xhttp.open("GET", "/assets/caps/1/", true);
-    xhttp.send();
-  }
 
-  function myFunction(xml) {
-    // console.log(xml.responseText)
-    var parser = new DOMParser();
-    var htmlDoc = parser.parseFromString(xml.responseText, 'text/html');
-    var preList = htmlDoc.getElementsByTagName("pre")[0].getElementsByTagName("a")
-    for (i = 1; i < preList.length; i++) {
-      console.log(preList[i].innerHTML)
-    }
+function lang_btn_event(el) {
+  let text =  el.querySelector('span#lang_btn_text');
+  if (text.innerHTML == 'en') {
+    text.innerHTML = 'ru'
+  } else {
+    text.innerHTML = 'en'
   }
-
-  console.log(loadDoc().responseText)
+}
